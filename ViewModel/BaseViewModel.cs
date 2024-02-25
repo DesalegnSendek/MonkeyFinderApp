49b3aab0872a941bc2296291a88d3682200a317a
DesalegnSendek <desalegnsendek2@gmail.com>
2024-02-25 21:29:10 +0300

    Internet

--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -1,5 +1,50 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+[INotifyPropertyChanged]
+public partial class BaseViewModel 
 {
+   public event PropertyChangedEventHandler PropertyChanged;
+    bool isBussy;
+    string title;
+
+    public BaseViewModel()
+    { 
+      // etProperty(ref title, "Title");
+    }
+    public bool IsBussy
+    {
+        get => isBussy;
+
+        set
+        {
+            if (isBussy == value)
+                return;
+
+            isBussy = value;
+
+           // OnPropertyChanged(nameof(IsBussy));
+            //OnPropertyChanged(nameof(isNotBussy));
+        }
+
+    }
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value) return;
+
+            title = value;
+            //OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    public bool isNotBussy => !IsBussy;
+
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
--- ViewModel/MonkeysViewModel.cs
+++ ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,47 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+using System.Linq.Expressions;
+namespace MonkeyFinder.ViewModel;
+
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        //Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+
+    }
+
+    async Task GetMonkeysAsync() 
+    {
+        if (IsBussy)
+            return;
+
+        try
+        {
+            IsBussy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+            if(monkeys.Count != 0)
+            monkeys.Clear();
+
+            foreach(var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+            
+        }catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("error", $"Unable to get Monkeys {ex.Message}", "Ok");
+        }
+        finally
+        {
+            IsBussy = false;
+
+        }
+    }
+
 }
